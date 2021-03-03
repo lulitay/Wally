988b66ade68ba66cc79762d68aa7a305c6e372f0
Bianca Ritorto <bianritorto@fb.com>
2021-03-03 18:50:11 -0300

    Primera clase

---
+++ app/src/main/java/com/example/pam_app/FTUActivity.java
@@ -0,0 +1,15 @@
+package com.example.pam_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FTUActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.ftu_activity);
+    }
+}
--- app/src/main/java/com/example/pam_app/MainActivity.java
+++ app/src/main/java/com/example/pam_app/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.pam_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SP_ID = "demo-pref";
+    private static final String FTU = "ftu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SharedPreferences sharedPreferences = getSharedPreferences(SP_ID, MODE_PRIVATE);
+        if (sharedPreferences.getBoolean(FTU, true)) {
+            sharedPreferences.edit().putBoolean(FTU, false).apply();
+            startActivity(new Intent(this, FTUActivity.class));
+        }
     }
 }
