b5630ed67f07425636442e02d9969163fb9b19a6
Bianca Ritorto <bianritorto@fb.com>
2021-03-10 19:01:50 -0300

    Segunda clase

--- app/src/main/java/com/example/pam_app/landing/LandingActivity.java
+++ app/src/main/java/com/example/pam_app/landing/LandingActivity.java
@@ -1,12 +1,13 @@
-package com.example.pam_app;
+package com.example.pam_app.landing;
 
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FTUActivity extends AppCompatActivity {
+import com.example.pam_app.R;
 
+public class LandingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle bundle) {
         super.onCreate(bundle);
