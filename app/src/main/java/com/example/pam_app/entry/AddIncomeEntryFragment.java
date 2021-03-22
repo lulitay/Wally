950c52c94b120756243574fa1b0b5c9e67820eb7
Bianca Ritorto <bianritorto@fb.com>
2021-03-22 00:14:44 -0300

    First forms

--- app/src/main/java/com/example/pam_app/entry/AddIncomeEntryFragment.java
+++ app/src/main/java/com/example/pam_app/entry/AddIncomeEntryFragment.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.pam_app.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AddIncomeEntryFragment extends Fragment {
     public static final String ARG_OBJECT = "object";
+    private static final List<String> BUCKETS = new ArrayList<>(Arrays.asList("Salary", "Bonus"));
 
     public AddIncomeEntryFragment() {
         // Required empty public constructor
@@ -21,6 +29,16 @@ public class AddIncomeEntryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_income, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.list_item, BUCKETS);
+
+        AutoCompleteTextView editTextFilledExposedDropdown = view.findViewById(R.id.auto_complete);
+        editTextFilledExposedDropdown.setAdapter(adapter);
     }
 
     public static AddIncomeEntryFragment newInstance(Integer counter) {
--- app/src/main/java/com/example/pam_app/entry/AddSpendingEntryFragment.java
+++ app/src/main/java/com/example/pam_app/entry/AddSpendingEntryFragment.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.pam_app.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AddSpendingEntryFragment extends Fragment {
     public static final String ARG_OBJECT = "object";
+    private static final List<String> BUCKETS = new ArrayList<>(Arrays.asList("Food", "Entertainment", "Gasoline"));
 
     public AddSpendingEntryFragment() {
         // Required empty public constructor
@@ -21,6 +29,16 @@ public class AddSpendingEntryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_spending, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.list_item, BUCKETS);
+
+        AutoCompleteTextView editTextFilledExposedDropdown = view.findViewById(R.id.auto_complete);
+        editTextFilledExposedDropdown.setAdapter(adapter);
     }
 
     public static AddSpendingEntryFragment newInstance(int counter) {
