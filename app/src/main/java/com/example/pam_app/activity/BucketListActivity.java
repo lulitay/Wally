411f2b64de34e2706983333be260d22e50083fbf
lulitay <ltay@itba.edu.ar>
2021-04-14 23:23:33 -0300

    Add entry button now opens AddEntryActivity

--- app/src/main/java/com/example/pam_app/activity/BucketListActivity.java
+++ app/src/main/java/com/example/pam_app/activity/BucketListActivity.java
@@ -1,38 +1,37 @@
 package com.example.pam_app.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
 import com.example.pam_app.R;
+import com.example.pam_app.MainActivity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class BucketListActivity extends AppCompatActivity {
-    ListView listview;
     Button addButton;
-    EditText GetValue;
-    String[] ListElements = new String[]{};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bucket_list);
 
-        addButton = findViewById(R.id.button1);
-
+        addButton = findViewById(R.id.add_entry_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addBucket(v);
             }
         });
+
     }
+
+    public void addBucket(View view) {
+        Intent intent = new Intent(this, AddEntryActivity.class);
+        startActivity(intent);
+    }
+
 }
