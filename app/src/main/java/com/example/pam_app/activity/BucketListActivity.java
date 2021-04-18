19d13cf26a12c69ae708a085fe103fa132d7d90a
lulitay <ltay@itba.edu.ar>
2021-04-18 15:51:05 -0300

    Bucket list view now displays buckets dynamically

--- app/src/main/java/com/example/pam_app/activity/BucketListActivity.java
+++ app/src/main/java/com/example/pam_app/activity/BucketListActivity.java
@@ -1,8 +1,8 @@
 package com.example.pam_app.activity;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -11,15 +11,13 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.pam_app.R;
-import com.example.pam_app.MainActivity;
+import com.example.pam_app.adapter.BucketListAdapter;
 import com.example.pam_app.model.Bucket;
 import com.example.pam_app.model.BucketType;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
 
 public class BucketListActivity extends AppCompatActivity {
     Button addBucketButton;
@@ -27,10 +25,7 @@ public class BucketListActivity extends AppCompatActivity {
 
     private Bucket savingsBucket = new Bucket("Car", Date.valueOf("2021-06-12"), BucketType.SAVING, 20000.0, null);
     private Bucket spendingBucket = new Bucket("Food", Date.valueOf("2021-06-12"), BucketType.SPENDING, 10000.0, null);
-    private List<Bucket> bucketList= new ArrayList(Arrays.asList(savingsBucket, spendingBucket));
-
-
-
+    private ArrayList<Bucket> bucketList = new ArrayList(Arrays.asList(savingsBucket, spendingBucket));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +41,13 @@ public class BucketListActivity extends AppCompatActivity {
             }
         });
 
-        final ArrayAdapter adapter = new ArrayAdapter<>
-                (BucketListActivity.this, android.R.layout.simple_list_item_1, bucketList);
+        final ArrayAdapter<Bucket> adapter = new BucketListAdapter
+                (this, bucketList);
         bucketListView.setAdapter(adapter);
-
     }
 
     public void addBucket(View view) {
         Intent intent = new Intent(this, AddEntryActivity.class);
         startActivity(intent);
     }
-
 }
---
+++ app/src/main/java/com/example/pam_app/adapter/BucketListAdapter.java
@@ -0,0 +1,51 @@
+package com.example.pam_app.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.pam_app.R;
+import com.example.pam_app.model.Bucket;
+
+import java.util.ArrayList;
+
+public class BucketListAdapter extends ArrayAdapter<Bucket> {
+    private final Activity context;
+    private final ArrayList<Bucket> bucketList;
+
+    static class ViewHolder {
+        public TextView title;
+        public TextView target;
+    }
+
+    public BucketListAdapter(Activity context, ArrayList<Bucket> bucketList) {
+        super(context, R.layout.bucket_list_item, bucketList);
+        this.context = context;
+        this.bucketList = bucketList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        // reuse views
+        if (rowView == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            rowView = inflater.inflate(R.layout.bucket_list_item, null);
+            // configure view holder
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.title = (TextView) rowView.findViewById(R.id.bucket_title);
+            viewHolder.target = (TextView) rowView.findViewById(R.id.bucket_target);
+            rowView.setTag(viewHolder);
+        }
+
+        // fill data
+        ViewHolder holder = (ViewHolder) rowView.getTag();
+        Bucket currentBucket = bucketList.get(position);
+        holder.title.setText(currentBucket.title);
+        holder.target.setText(String.valueOf(currentBucket.target));
+        return rowView;
+    }
+}
