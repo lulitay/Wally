4b244cc806ae19dd01866f5eceb8f3d5d18fd846
lulitay <ltay@itba.edu.ar>
2021-04-18 13:38:32 -0300

    Added list to bucket list view

--- app/src/main/java/com/example/pam_app/activity/BucketListActivity.java
+++ app/src/main/java/com/example/pam_app/activity/BucketListActivity.java
@@ -1,31 +1,54 @@
 package com.example.pam_app.activity;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.pam_app.R;
 import com.example.pam_app.MainActivity;
+import com.example.pam_app.model.Bucket;
+import com.example.pam_app.model.BucketType;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class BucketListActivity extends AppCompatActivity {
-    Button addButton;
+    Button addBucketButton;
+    ListView bucketListView;
+
+    private Bucket savingsBucket = new Bucket("Car", Date.valueOf("2021-06-12"), BucketType.SAVING, 20000.0, null);
+    private Bucket spendingBucket = new Bucket("Food", Date.valueOf("2021-06-12"), BucketType.SPENDING, 10000.0, null);
+    private List<Bucket> bucketList= new ArrayList(Arrays.asList(savingsBucket, spendingBucket));
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bucket_list);
 
-        addButton = findViewById(R.id.add_entry_button);
-        addButton.setOnClickListener(new View.OnClickListener() {
+        bucketListView = findViewById(R.id.listView);
+        addBucketButton = findViewById(R.id.add_entry_button);
+        addBucketButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addBucket(v);
             }
         });
+
+        final ArrayAdapter adapter = new ArrayAdapter<>
+                (BucketListActivity.this, android.R.layout.simple_list_item_1, bucketList);
+        bucketListView.setAdapter(adapter);
 
     }
 
