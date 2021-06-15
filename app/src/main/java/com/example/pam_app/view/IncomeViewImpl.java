f6ebc80a65e417f959908b705e07586f9cde5626
Bianca Ritorto <bianritorto@fb.com>
2021-06-15 16:22:55 -0300

    Eye button added

--- app/src/main/java/com/example/pam_app/view/IncomeViewImpl.java
+++ app/src/main/java/com/example/pam_app/view/IncomeViewImpl.java
@@ -2,6 +2,7 @@ package com.example.pam_app.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
@@ -23,6 +24,10 @@ public class IncomeViewImpl extends LinearLayout implements IncomeView {
     private IncomeAdapter adapter;
     private final IncomePresenter incomePresenter;
     private Double incomeLeft;
+    private final ImageView eye;
+    private MaterialTextView incomeLeftText;
+
+    private boolean showAmount;
 
     public IncomeViewImpl(Context context) {
         this(context, null);
@@ -37,15 +42,27 @@ public class IncomeViewImpl extends LinearLayout implements IncomeView {
         inflate(context, R.layout.view_income, this);
         setOrientation(VERTICAL);
         this.incomePresenter = new IncomePresenter(this);
+        this.showAmount = false;
         this.context = context;
         this.emptyNotice = findViewById(R.id.income_unavailable);
         this.incomeLeft = 0.0;
+        this.eye = findViewById(R.id.eye);
+        this.incomeLeftText = findViewById(R.id.income_left);
 
         setUpList();
+        setOnEyeClickListener();
+    }
+
+    private void setOnEyeClickListener() {
+        eye.setOnClickListener(v -> {
+            showAmount = !showAmount;
+            setUpEye();
+        });
     }
 
     @Override
     public void bind(final List<Income> incomeList, final Double incomeLeft) {
+        setUpEye();
         if (incomeList.isEmpty()) {
             emptyNotice.setVisibility(VISIBLE);
             this.incomeLeft = 0.0;
@@ -75,12 +92,13 @@ public class IncomeViewImpl extends LinearLayout implements IncomeView {
 
     @Override
     public void setUpIncomeLeftText(final boolean isPositive) {
-        final MaterialTextView incomeLeftText = findViewById(R.id.income_left);
-        incomeLeftText.setText(context.getString(R.string.money_display, this.incomeLeft));
-        incomeLeftText.setTextColor(
-                isPositive ? context.getResources().getColor(R.color.green, context.getTheme()) :
-                        context.getResources().getColor(R.color.red, context.getTheme())
-        );
+        if (showAmount) {
+            incomeLeftText.setText(context.getString(R.string.money_display, this.incomeLeft));
+            incomeLeftText.setTextColor(
+                    isPositive ? context.getResources().getColor(R.color.green, context.getTheme()) :
+                            context.getResources().getColor(R.color.red, context.getTheme())
+            );
+        }
     }
 
     private void setUpList() {
@@ -88,5 +106,16 @@ public class IncomeViewImpl extends LinearLayout implements IncomeView {
         adapter = new IncomeAdapter();
         listView.setAdapter(adapter);
         ViewCompat.setNestedScrollingEnabled(listView, false);
+    }
+
+    private void setUpEye() {
+        if (showAmount) {
+            eye.setImageResource(R.drawable.ic_nonvisibility);
+            setUpIncomeLeftText(incomeLeft > 0);
+        } else {
+            eye.setImageResource(R.drawable.ic_visibility);
+            incomeLeftText.setText("****.**");
+            incomeLeftText.setTextColor(context.getResources().getColor(R.color.highlightText, context.getTheme()));
+        }
     }
 }
